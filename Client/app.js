2ce101c767b892fef050ea1dbbd49fb5e63a9b7e
Kenneth Wong <kenneth.wong8(.a.t.)gmail.com>
2017-06-13 09:07:37 -0700

    Refactoring

--- Client/app.js
+++ Client/app.js
@@ -34,42 +34,7 @@ $(document).ready(function() {
 				},
 				method:'GET',
 				success: function(data){
-					$('#mojio-User').append(
-						`
-						<table class='table table-striped table-hover table-responsive' id='userTable' style="width:100%">
-							<thead class="thead-default">
-								<tr>
-									<th style='display:none;'>Id</th>
-									<th>User Name</th>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Email</th>
-									<th>Update</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class='id' style='display:none;'>
-										<input class="form-control input-sm" type="text" value='${data.Id}'>
-									</td>
-									<td class='un'>
-										<input class="form-control input-sm" type="text" value='${data.UserName}'>
-									</td>
-									<td class='fn'>
-										<input class="form-control input-sm" type="text" value='${data.FirstName}'>
-									</td>
-									<td class='ln'>
-										<input class="form-control input-sm" type="text" value='${data.LastName}'>
-									</td>
-									<td class='em'>
-										<input class="form-control input-sm" type="text" value='${data.Emails[0].Address}' disabled>
-									</td>
-									<td><button class='btn btn-success update-user'>Update</button></td>
-								</tr>
-							</tbody>
-						</table>
-						`
-					);
+					createUserTable(data);
 				},
 				error: function(xhr, text, err){
 					console.log(xhr.responseJSON.Message);
@@ -86,26 +51,11 @@ $(document).ready(function() {
 
 	// [PUT] USER ID
 	$('body').on('click', 'button.update-user', function(){
-		let id = $(this).closest("tr")
-			.find('.id')
-			.find('input')
-			.val();
-		let userName = $(this).closest("tr")
-			.find('.un')
-			.find('input')
-			.val();
-		let firstName = $(this).closest("tr")
-			.find('.fn')
-			.find('input')
-			.val();
-		let lastName = $(this).closest("tr")
-			.find('.ln')
-			.find('input')
-			.val();
-		let email = $(this).closest("tr")
-			.find('.em')
-			.find('input')
-			.val();
+		let id = $(this).closest("tr").find('.id').find('input').val();
+		let userName = $(this).closest("tr").find('.un').find('input').val();
+		let firstName = $(this).closest("tr").find('.fn').find('input').val();
+		let lastName = $(this).closest("tr").find('.ln').find('input').val();
+		let email = $(this).closest("tr").find('.em').find('input').val();
 		// Ajax call to save the entity
 		let apiToken = $('#apiToken').val();
 		if (apiToken){
@@ -141,14 +91,9 @@ $(document).ready(function() {
 	});
 
 	// [GET] MOJIO
-	let ajaxGetMojioInProgress = false;
 	$('#getMojios').on('click', function(){
-		if (ajaxGetMojioInProgress){
-			return;
-		}
 		let apiToken = $('#apiToken').val();
 		if (apiToken){
-			ajaxGetMojioInProgress = true;
 			$.ajax({
 				url:'https://api.moj.io/v2/Mojios',
 				headers:{
@@ -157,61 +102,13 @@ $(document).ready(function() {
 				},
 				method:'GET',
 				success: function(data){
-					if (data.Data.length>0){
-						$('#mojio-Mojios').append(
-								`
-								<table class='table table-striped table-hover table-responsive' id='mojioTable' style="width:100%">
-									<thead class="thead-default">
-										<tr>
-											<th style='display:none;'>ID</th>
-											<th>IMEI</th>
-											<th>Name</th>
-											<th>LastContactTime</th>
-											<th style='display:none;'>Vehicle Id</th>
-											<th>Update</th>
-											<th>Remove Device</th>
-										</tr>
-									</thead>
-									<tbody>
-									</tbody>
-								</table>
-								`
-							);
-						data.Data.forEach(function(mojio){
-							$('#mojioTable > tbody:last-child').append(
-								`
-								<tr>
-									<td class='id' style='display:none;'>
-										<input class="form-control input-sm" type="text" value='${mojio.Id}'>
-									</td>
-									<td class='imei'>
-										<input class="form-control input-sm" type="text" value='${mojio.IMEI}' disabled>
-									</td>
-									<td class='n'>
-										<input class="form-control input-sm" type="text" value='${mojio.Name}'>
-									</td>
-									<td class='lct'>
-										<input class="form-control input-sm" type="text" value='${mojio.LastContactTime}' disabled>
-									</td>
-									<td class='vid' style='display:none;'>
-										<input class="form-control input-sm" type="text" value='${mojio.VehicleId}'>
-									</td>
-									<td><button class='btn btn-success update-mojio'>Update</button></td>
-									<td class='remove-mojio'><button type="button" class="btn btn-outline-danger remove-mojio-button" id="removeMojioButton">Remove</button></td>
-
-								</tr>
-								`
-							);
-						});
-					}
-					ajaxGetMojioInProgress = false;
+					createMojioTable(data);
 				},
 				error: function(xhr, text, err){
 					console.log(xhr.responseJSON.Message);
 					console.log(xhr.status);
 					console.log(xhr.statusCode);
 					errorAlert('Oh snap!', 'Mojio API Token is not valid.');
-					ajaxGetMojioInProgress = false;
 				}
 			});
 
@@ -223,26 +120,11 @@ $(document).ready(function() {
 
 	// [PUT] MOJIO ID
 	$('body').on('click', 'button.update-mojio', function(){
-		let id = $(this).closest("tr")
-			.find(".id")
-			.find('input')
-			.val();
-		let imei = $(this).closest("tr")
-			.find(".imei")
-			.find('input')
-			.val();
-		let n = $(this).closest("tr")
-			.find(".n")
-			.find('input')
-			.val();
-		let lct = $(this).closest("tr")
-			.find(".lct")
-			.find('input')
-			.val();
-		let vid = $(this).closest("tr")
-			.find(".vid")
-			.find('input')
-			.val();
+		let id = $(this).closest("tr").find(".id").find('input').val();
+		let imei = $(this).closest("tr").find(".imei").find('input').val();
+		let n = $(this).closest("tr").find(".n").find('input').val();
+		let lct = $(this).closest("tr").find(".lct").find('input').val();
+		let vid = $(this).closest("tr").find(".vid").find('input').val();
 		// Ajax call to save the entity
 		let apiToken = $('#apiToken').val();
 		if (apiToken){
@@ -282,7 +164,7 @@ $(document).ready(function() {
 
 		bootbox.confirm({
 			title: "Unclaiming a Mojio!",
-			message: `Are you sure you want to unclaim Mojio: ${imei}`,
+			message: `Are you sure you want to unclaim Mojio with IMEI - ${imei}?`,
 			buttons: {
 				cancel: {
 					label: '<i class="fa fa-times"></i> Cancel'
@@ -338,99 +220,7 @@ $(document).ready(function() {
 				},
 				method:'GET',
 				success: function(data){
-					if (data.Data.length>0){
-						$('#mojio-Vehicles').append(
-								`
-								<table class='table table-striped table-hover table-responsive' id='vehicleTable' style="width:100%">
-									<thead class="thead-default">
-										<tr>
-											<th>Merge</th>
-											<th style='display:none;'>ID</th>
-											<th style='display:none;'>MojioID</th>
-											<th>Name</th>
-											<th>License Plate</th>
-											<th>VIN</th>
-											<th>Odometer (KM)</th>
-											<th>Driving</th>
-											<th>Deleted</th>
-											<th>Update</th>
-											<th>Delete</th>
-										</tr>
-									</thead>
-									<tbody>
-									</tbody>
-								</table>
-								`
-							);
-						data.Data.forEach(function(vehicle){
-							let id = vehicle.Id;
-							let mojid = vehicle.MojioId;
-							let vehicleName = vehicle.Name;
-							let vehicleLicensePlate = vehicle.LicensePlate;
-							let vehicleVin = vehicle.VIN;
-							let vehicleOdoObj = vehicle.Odometer;
-							let vehicleOdo = 'N/A'
-							if (typeof vehicleOdoObj != 'undefined'){
-								vehicleOdo = vehicle.Odometer.Value / 1000; // Convert to KM
-							}
-							
-							let vehicleIgnitionState = vehicle.IgnitionState.Value === false ? 'Parked' : 'Driving';
-							let vehicleDeleted = vehicle.Deleted === false ? 'No' : 'Yes';
-							$('#vehicleTable > tbody:last-child').append(
-								`
-								<tr>
-									<td class='merge'>
-										<div class="input-group">
-											<span class="input-group-addon">
-												<input id='mergeCheckBox' type="checkbox" aria-label="Checkbox for following text input">
-											</span>
-										</div>
-									</td>
-									<td class='id' style='display:none;'>
-										<input class="form-control input-sm" type="text" value='${id}'>
-									</td>
-									<td class='mojid' style='display:none;'>
-										<input class="form-control input-sm" type="text" value='${mojid}'>
-									</td>
-									<td class='n'>
-										<input class="form-control input-sm" type="text" value='${vehicleName}'>
-									</td>
-									<td class='lc'>
-										<input class="form-control input-sm" type="text" value='${vehicleLicensePlate}'>
-									</td>
-									<td class='vin'>
-										<input class="form-control input-sm" type="text" value='${vehicleVin}' disabled>
-									</td>
-									<td class='odo'>
-										<input class="form-control input-sm" type="text" value='${vehicleOdo}'>
-									</td>
-									<td class='ign'>
-										<input class="form-control input-sm" type="text" value='${vehicleIgnitionState}' disabled>
-									</td>
-									<td class='d'>
-										<input class="form-control input-sm" type="text" value='${vehicleDeleted}' disabled>
-									</td>
-									<td><button class='btn btn-success update-vehicle'>Update</button></td>
-									<td><button class='btn btn-outline-danger delete-vehicle'>Delete</button></td>
-								</tr>
-								`
-							);
-							if (mojid){
-								$('.delete-vehicle').attr('disabled', 'disabled');
-								$('.delete-vehicle').attr('title', 'This vehicle is connected to a Mojio');
-							}
-						});
-						// $('#mojio-Vehicles').append(
-						// 		`<button type="button" class="btn btn-outline-danger">Merge</button>`
-						// );
-						$('#vehicleTable > tbody:last-child').append(
-							`
-							<tr>
-								<td class='merge-button'><button type="button" class="btn btn-outline-danger merge-vehicle" id="mergeButton" disabled>Merge</button></td>
-							</tr>
-							`
-						)
-					}
+					createVehicleTable(data);
 				},
 				error: function(xhr, text, err){
 					console.log(xhr.responseJSON.Message);
@@ -447,39 +237,15 @@ $(document).ready(function() {
 
 	// [PUT] VEHICILE ID
 	$('body').on('click', 'button.update-vehicle', function(){
-		let id = $(this).closest("tr")
-			.find(".id")
-			.find('input')
-			.val();
-		let mojid = $(this).closest("tr")
-			.find(".mojid")
-			.find('input')
-			.val();
-		let n = $(this).closest("tr")
-			.find(".n")
-			.find('input')
-			.val();
-		let lc = $(this).closest("tr")
-			.find(".lc")
-			.find('input')
-			.val();
-		let vin = $(this).closest("tr")
-			.find(".vin")
-			.find('input')
-			.val();
-		let odo = $(this).closest("tr")
-			.find(".odo")
-			.find('input')
-			.val();
+		let id = $(this).closest("tr").find(".id").find('input').val();
+		let mojid = $(this).closest("tr").find(".mojid").find('input').val();
+		let n = $(this).closest("tr").find(".n").find('input').val();
+		let lc = $(this).closest("tr").find(".lc").find('input').val();
+		let vin = $(this).closest("tr").find(".vin").find('input').val();
+		let odo = $(this).closest("tr").find(".odo").find('input').val();
 		odo *= 1000; // Convert to meters
-		let ign = $(this).closest("tr")
-			.find(".ign")
-			.find('input')
-			.val();
-		let del = $(this).closest("tr")
-			.find(".d")
-			.find('input')
-			.val();
+		let ign = $(this).closest("tr").find(".ign").find('input').val();
+		let del = $(this).closest("tr").find(".d").find('input').val();
 		// Ajax call to save the entity
 		let apiToken = $('#apiToken').val();
 		if (apiToken){
@@ -600,6 +366,7 @@ $(document).ready(function() {
 	});
 
 	$('#btnClaimMojio').on('click', function(){
+		let id = $(this).closest("tr").find(".id").find('input').val();
 		let imeiString = $('#textBulkClaimImei').val();
 		let imeiArray = imeiString.split(',');
 		imeiArray.forEach(function(imei){
@@ -678,6 +445,193 @@ $(document).ready(function() {
 			$('#error-message').html('');
 		});
 	};
+
+	let createUserTable = function(data){
+		$('#mojio-User').append(
+			`
+			<table class='table table-striped table-hover table-responsive' id='userTable' style="width:100%">
+				<thead class="thead-default">
+					<tr>
+						<th style='display:none;'>Id</th>
+						<th>User Name</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>Update</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class='id' style='display:none;'>
+							<input class="form-control input-sm" type="text" value='${data.Id}'>
+						</td>
+						<td class='un'>
+							<input class="form-control input-sm" type="text" value='${data.UserName}'>
+						</td>
+						<td class='fn'>
+							<input class="form-control input-sm" type="text" value='${data.FirstName}'>
+						</td>
+						<td class='ln'>
+							<input class="form-control input-sm" type="text" value='${data.LastName}'>
+						</td>
+						<td class='em'>
+							<input class="form-control input-sm" type="text" value='${data.Emails[0].Address}' disabled>
+						</td>
+						<td><button class='btn btn-success update-user'>Update</button></td>
+					</tr>
+				</tbody>
+			</table>
+			`
+		);
+	};
+	
+	let createMojioTable = function(data){
+		if (data.Data.length>0){
+			$('#mojio-Mojios').append(
+					`
+					<table class='table table-striped table-hover table-responsive' id='mojioTable' style="width:100%">
+						<thead class="thead-default">
+							<tr>
+								<th style='display:none;'>ID</th>
+								<th>IMEI</th>
+								<th>Name</th>
+								<th>LastContactTime</th>
+								<th style='display:none;'>Vehicle Id</th>
+								<th>Update</th>
+								<th>Remove Device</th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+					`
+				);
+			data.Data.forEach(function(mojio){
+				$('#mojioTable > tbody:last-child').append(
+					`
+					<tr>
+						<td class='id' style='display:none;'>
+							<input class="form-control input-sm" type="text" value='${mojio.Id}'>
+						</td>
+						<td class='imei'>
+							<input class="form-control input-sm" type="text" value='${mojio.IMEI}' disabled>
+						</td>
+						<td class='n'>
+							<input class="form-control input-sm" type="text" value='${mojio.Name}'>
+						</td>
+						<td class='lct'>
+							<input class="form-control input-sm" type="text" value='${mojio.LastContactTime}' disabled>
+						</td>
+						<td class='vid' style='display:none;'>
+							<input class="form-control input-sm" type="text" value='${mojio.VehicleId}'>
+						</td>
+						<td><button class='btn btn-success update-mojio'>Update</button></td>
+						<td class='remove-mojio'><button type="button" class="btn btn-outline-danger remove-mojio-button" id="removeMojioButton">Remove</button></td>
+
+					</tr>
+					`
+				);
+			});
+		}
+	};
+	
+	let createVehicleTable = function(data){
+		if (data.Data.length>0){
+			$('#mojio-Vehicles').append(
+					`
+					<table class='table table-striped table-hover table-responsive' id='vehicleTable' style="width:100%">
+						<thead class="thead-default">
+							<tr>
+								<th>Merge</th>
+								<th style='display:none;'>ID</th>
+								<th style='display:none;'>MojioID</th>
+								<th>Name</th>
+								<th>License Plate</th>
+								<th>VIN</th>
+								<th>Odometer (KM)</th>
+								<th>Driving</th>
+								<th>Deleted</th>
+								<th>Update</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+					`
+				);
+			data.Data.forEach(function(vehicle){
+				let id = vehicle.Id;
+				let mojid = vehicle.MojioId;
+				let vehicleName = vehicle.Name;
+				let vehicleLicensePlate = vehicle.LicensePlate;
+				let vehicleVin = vehicle.VIN;
+				let vehicleOdoObj = vehicle.Odometer;
+				let vehicleOdo = 'N/A'
+				if (typeof vehicleOdoObj != 'undefined'){
+					vehicleOdo = vehicle.Odometer.Value / 1000; // Convert to KM
+				}
+				
+				let vehicleIgnitionState = vehicle.IgnitionState.Value === false ? 'Parked' : 'Driving';
+				let vehicleDeleted = vehicle.Deleted === false ? 'No' : 'Yes';
+				$('#vehicleTable > tbody:last-child').append(
+					`
+					<tr>
+						<td class='merge'>
+							<div class="input-group">
+								<span class="input-group-addon">
+									<input id='mergeCheckBox' type="checkbox" aria-label="Checkbox for following text input">
+								</span>
+							</div>
+						</td>
+						<td class='id' style='display:none;'>
+							<input class="form-control input-sm" type="text" value='${id}'>
+						</td>
+						<td class='mojid' style='display:none;'>
+							<input class="form-control input-sm" type="text" value='${mojid}'>
+						</td>
+						<td class='n'>
+							<input class="form-control input-sm" type="text" value='${vehicleName}'>
+						</td>
+						<td class='lc'>
+							<input class="form-control input-sm" type="text" value='${vehicleLicensePlate}'>
+						</td>
+						<td class='vin'>
+							<input class="form-control input-sm" type="text" value='${vehicleVin}' disabled>
+						</td>
+						<td class='odo'>
+							<input class="form-control input-sm" type="text" value='${vehicleOdo}'>
+						</td>
+						<td class='ign'>
+							<input class="form-control input-sm" type="text" value='${vehicleIgnitionState}' disabled>
+						</td>
+						<td class='d'>
+							<input class="form-control input-sm" type="text" value='${vehicleDeleted}' disabled>
+						</td>
+						<td><button class='btn btn-success update-vehicle'>Update</button></td>
+						<td><button class='btn btn-outline-danger delete-vehicle'>Delete</button></td>
+					</tr>
+					`
+				);
+				if (mojid){
+					$('.delete-vehicle').attr('disabled', 'disabled');
+					$('.delete-vehicle').attr('title', 'This vehicle is connected to a Mojio');
+				}
+			});
+			// $('#mojio-Vehicles').append(
+			// 		`<button type="button" class="btn btn-outline-danger">Merge</button>`
+			// );
+			$('#vehicleTable > tbody:last-child').append(
+				`
+				<tr>
+					<td class='merge-button'><button type="button" class="btn btn-outline-danger merge-vehicle" id="mergeButton" disabled>Merge</button></td>
+				</tr>
+				`
+			)
+		}
+	};
+
+	
 });
 
 $( document ).ajaxError(function( event, request, settings ) {
